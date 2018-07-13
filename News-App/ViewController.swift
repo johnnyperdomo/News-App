e72ad532d7467cf6b1afb13e39e25c2332fe9fe4
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-07-13 19:53:39 -0400

    install pods

--- News-App/ViewController.swift
+++ News-App/ViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+
 
 class ViewController: UIViewController {
 
@@ -15,11 +19,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'News-App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for News-App
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'Alamofire', '~> 4.6'
+end
